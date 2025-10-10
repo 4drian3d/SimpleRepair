6be8266b0e68248f9352c019a5d974a323b81aec
Adrian Gonzales <adriangonzalesval@gmail.com>
2025-10-10 16:59:26 -0500

    fix: Fixed default nonRepairableItem color

--- common/src/main/java/io/github/_4drian3d/simplerepair/common/configuration/Configuration.java
+++ common/src/main/java/io/github/_4drian3d/simplerepair/common/configuration/Configuration.java
@@ -8,7 +8,7 @@ public class Configuration {
     @Comment("Message to send when you want to repair an already repaired item")
     private String alreadyRepaired = "<red>The item is already repaired";
     @Comment("Message to be sent when the item cannot be repaired")
-    private String nonRepairableItem = "This item cannot be repaired";
+    private String nonRepairableItem = "<red>This item cannot be repaired";
     @Comment("Message to be sent when the item has been repaired successfully")
     private String itemRepaired = "<green>The item was repaired";
 
